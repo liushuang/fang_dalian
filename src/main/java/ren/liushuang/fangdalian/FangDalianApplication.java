f6074714359668c8b953fc46ecae331116cb62bc
liushuang <liu.shuang@linecorp.com>
2019-08-19 16:03:38 +0800

    + add code

---
+++ src/main/java/ren/liushuang/fangdalian/FangDalianApplication.java
@@ -0,0 +1,13 @@
+package ren.liushuang.fangdalian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FangDalianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FangDalianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ren/liushuang/fangdalian/FangDalianApplicationTests.java
@@ -0,0 +1,16 @@
+package ren.liushuang.fangdalian;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FangDalianApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
